2f883eea44645b00f9814edb2323a2dba65a945b
quelz <iamquel08@gmail.com>
2018-03-03 19:20:50 +0200

    Parsing fb

--- src/main/java/com/infofromquel/entity/EmailTemplates.java
+++ src/main/java/com/infofromquel/entity/EmailTemplates.java
@@ -2,12 +2,23 @@ package com.infofromquel.entity;
 
 
 
-public abstract class EmailTemplates {
+public enum EmailTemplates {
+    ;
 
-    public static String REGISTRATION_TEMPLATE = "<h3 style='color:red' >Hello World!</h3>" +
+    private String template;
+
+    EmailTemplates(String template) {
+        this.template = template;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    String REGISTRATION_TEMPLATE = "<h3 style='color:red' >Hello World!</h3>" +
                                                  "<a href=0>Please follow this link to continue registration </a>";
 
-    public static String REGISTRATION_SUBJECT = "Thanks for your registration on InfoQvel ";
+    String REGISTRATION_SUBJECT = "Thanks for your registration on InfoQvel ";
 
 
 }
